53a23dcbb5074bd705d81f39d1d39c7a38af8261
Abhigyan1997 <alokabhigyan65@gmail.com>
2025-07-02 21:12:44 +0530

    cors error

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const documentRoutes = require('./routes/documents');
 const paymentRoutes = require('./routes/paymentRoute');
 require('dotenv').config();
 
-
 app.use(express.json());
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://nyaymitra-landing-page-lang-v1.vercel.app'],
     credentials: true
 }));
-app.use('/api/documents', documentRoutes);
 
+app.use('/api/documents', documentRoutes);
 app.use('/api/payment', paymentRoutes);
 
 module.exports = app;
